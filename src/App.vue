728a130786661c82cbd5954f0b4aa64edd9deab1
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2020-10-28 14:41:16 +0100

    get course list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
+    <div v-for="course in coursesList" :key="course.slug">
+      {{course.title}}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
+  },
+  data: () => ({
+    coursesList: [],
+  }),
+  mounted() {
+    this.getCourses();
+  },
+  methods: {
+    async getCourses() {
+      try {
+        const host = process.env.VUE_APP_HOST;
+        const result = await axios(host);
+        this.coursesList = result.data;
+        console.log(this.coursesList);
+      } catch (err) {
+        console.error(err);
+      }
+    },
   },
 };
 </script>
